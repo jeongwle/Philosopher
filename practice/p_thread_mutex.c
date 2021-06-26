9b91d5b8bb4a0cd92357b6e043aa60919da81502
Jeongwook Lee <jeongwle@c3r5s7.42seoul.kr>
2021-06-26 18:52:42 +0900

    4

--- practice/p_thread_mutex.c
+++ practice/p_thread_mutex.c
@@ -4,18 +4,14 @@
  뮤텍스는 lock과 unlock 두개의 행동만이 정의되어 있다.
  lock을 통해서 임계영역을 진입하고 unlock을 통해서 임계영역을 빠져나오고
  다른 스레드가 임계영역에 들어갈 수 있게 한다.
-
  int pthread_mutex_lock(pthread_mutex_t *mutex);
  int pthread_mutex_unlock(pthread_mutex_t *mutex);
  int phtread_mutex_destroy(pthread_mutex_t *mutex);
  int phtread_mutex_init(phtread_mutex_t *mutex, const phtread_mutex_attr *attr);
-
  phtread_mutex_init은 항상 0을 리턴하고 나머지 함수들은 성공시 0 실패시 0이 아닌
  숫자를 반환하고 errno를 설정한다.
-
  뮤텍스는 fast, recurisev, error checking 3가지 종류중 하나를 선택할 수 있으며
  기본적으로는 fast가 사용된다.
-
  뮤텍스를 사용하기전에 초기화를 해주어야 하는데 두가지 방법이 있다.
  PHTREAD_MUTEX_INITIALIZER 상수를 이용해서 초기화 해주는 방법
  pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER; 이런식
@@ -25,7 +21,7 @@
 
 #include <pthread.h>
 #include <stdio.h>
-#include <unistd.H>
+#include <unistd.h>
 #include <stdlib.h>
 
 int	cnt;
@@ -39,13 +35,16 @@ void	*count(void *arg)
 	i = 0;
 	name = (char *)arg;
 	pthread_mutex_lock(&mutex);
+	cnt = 0;
 	while (i < 10)
 	{
 		printf("%s cnt : %d \n", name, cnt);
+		cnt++;
 		i++;
 		usleep(1);
 	}
 	pthread_mutex_unlock(&mutex);
+	return (NULL);
 }
 
 int	main(void)
@@ -58,5 +57,5 @@ int	main(void)
 	pthread_create(&thread2, NULL, count, (void *)"thread2");
 	pthread_join(thread1, NULL);
 	pthread_join(thread2, NULL);
-	pthread_destory(&mutex);
+	pthread_mutex_destroy(&mutex);
 }
