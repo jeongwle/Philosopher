e125c0c2a296155510cc53bc0613d594e2ce4a46
Jeongwook Lee <jeongwle@c3r5s7.42seoul.kr>
2021-06-29 20:49:00 +0900

    7

--- philo/main.c
+++ philo/main.c
@@ -6,23 +6,168 @@
 /*   By: jeongwle <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/27 14:09:54 by jeongwle          #+#    #+#             */
-/*   Updated: 2021/06/28 22:12:18 by jeongwle         ###   ########.fr       */
+/*   Updated: 2021/06/29 20:11:17 by jeongwle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-void	*take_fork(void *arg)
+
+void	take_left_fork(t_param *param, int num)
+{	
+	pthread_mutex_lock(&param->philo[num - 1].fork);
+	print_status(param, num, "has taken left fork");
+}
+
+void	take_right_fork(t_param *param, int num)
+{
+	if (num == 1)
+	{
+		pthread_mutex_lock(&param->philo[param->philo_num - 1].fork);
+		print_status(param, num, "has taken right fork");
+	}
+	else
+	{
+		pthread_mutex_lock(&param->philo[num - 2].fork);
+		print_status(param, num, "has taken right fork");
+	}
+}
+
+void	return_fork(t_param *param, int num)
+{
+	if (num == 1)
+	{
+		pthread_mutex_unlock(&param->philo[num - 1].fork);
+		pthread_mutex_unlock(&param->philo[param->philo_num - 1].fork);
+	}
+	else
+	{
+		pthread_mutex_unlock(&param->philo[num - 1].fork);
+		pthread_mutex_unlock(&param->philo[num - 2].fork);
+	}
+}
+
+void	*start_routine(void *arg)
 {
 	t_param	*param;
+	int		num;
 
-	param = (t_param *)arg;
-	int i = 0;
-	while (i < param->philo_num)
+	param = (t_param *)((t_philo *)arg)->data;
+	num = (int)((t_philo *)arg)->identifier;
+	if (num % 2 == 0)
+		ft_usleep(1);
+	while (1)
 	{
-		printf("%d\n", param->philo[i].identifier);
-		i++;
+		take_left_fork(param, num);
+		take_right_fork(param, num);
+		print_status(param, num, "is eating");
+		ft_usleep(400);
+		return_fork(param, num);
+		print_status(param, num, "is sleeping");
+		ft_usleep(200);
+		print_status(param, num, "is thinking");
 	}
+	/*
+	if (num == 1)
+	{
+		if (param->philo[param->philo_num - 1].left == 0)
+		{
+			param->philo[num - 1].right = 1;
+			pthread_mutex_lock(&param->philo[param->philo_num - 1].fork);
+			print_status(param, num, "has taken fork");
+			if (param->philo[num - 1].left == 1)
+			{
+				print_status(param, num, "is eating");
+				ft_usleep(400);
+				param->philo[num - 1].right = 0;
+				param->philo[num - 1].left = 0;
+				pthread_mutex_unlock(&param->philo[param->philo_num - 1].fork);
+				pthread_mutex_unlock(&param->philo[num - 1].fork);
+			}
+		}
+		if (param->philo[num].right == 0)
+		{
+			param->philo[num - 1].left = 1;
+			pthread_mutex_lock(&param->philo[num - 1].fork);
+			print_status(param, num, "has taken fork");
+			if (param->philo[num - 1].right == 1)
+			{
+				print_status(param, num, "is eating");
+				ft_usleep(400);
+				param->philo[num - 1].right = 0;
+				param->philo[num - 1].left = 0;
+				pthread_mutex_unlock(&param->philo[param->philo_num - 1].fork);
+				pthread_mutex_unlock(&param->philo[num - 1].fork);
+			}
+		}
+	}
+	else if (num == param->philo_num)
+	{
+		if (param->philo[num - 2].left == 0)
+		{
+			param->philo[num - 1].right = 1;
+			pthread_mutex_lock(&param->philo[num - 2].fork);
+			print_status(param, num, "has taken fork");
+			if (param->philo[num - 1].left == 1)
+			{
+				print_status(param, num, "is eating");
+				ft_usleep(400);
+				param->philo[num - 1].right = 0;
+				param->philo[num - 1].left = 0;
+				pthread_mutex_unlock(&param->philo[num - 2].fork);
+				pthread_mutex_unlock(&param->philo[num - 1].fork);
+			}
+		}
+		if (param->philo[0].right == 0)
+		{
+			param->philo[num - 1].left = 1;
+			pthread_mutex_lock(&param->philo[0].fork);
+			print_status(param, num, "has taken fork");
+			if (param->philo[num - 1].right == 1)
+			{
+				print_status(param, num, "is eating");
+				ft_usleep(400);
+				param->philo[num - 1].right = 0;
+				param->philo[num - 1].left = 0;
+				pthread_mutex_unlock(&param->philo[num - 2].fork);
+				pthread_mutex_unlock(&param->philo[num - 1].fork);
+			}
+		}
+	}
+	else
+	{
+		if (param->philo[num - 2].left == 0)
+		{
+			param->philo[num - 1].right = 1;
+			pthread_mutex_lock(&param->philo[num - 2].fork);
+			print_status(param, num, "has taken fork");
+			if (param->philo[num - 1].left == 1)
+			{
+				print_status(param, num, "is eating");
+				ft_usleep(400);
+				param->philo[num - 1].right = 0;
+				param->philo[num - 1].left = 0;
+				pthread_mutex_unlock(&param->philo[num - 2].fork);
+				pthread_mutex_unlock(&param->philo[num - 1].fork);
+			}
+		}
+		if (param->philo[num].right == 0)
+		{
+			param->philo[num - 1].left = 1;
+			pthread_mutex_lock(&param->philo[num - 1].fork);
+			print_status(param, num, "has taken fork");
+			if (param->philo[num - 1].right == 1)
+			{
+				print_status(param, num, "is eating");
+				ft_usleep(400);
+				param->philo[num - 1].right = 0;
+				param->philo[num - 1].left = 0;
+				pthread_mutex_unlock(&param->philo[num - 2].fork);
+				pthread_mutex_unlock(&param->philo[num - 1].fork);
+			}
+		}
+	}*/
+	return (NULL);
 }
 
 void	create_thread(t_param *param)
@@ -30,12 +175,22 @@ void	create_thread(t_param *param)
 	int	i;
 
 	i = 0;
+	gettimeofday(&param->old_time, NULL);
 	while (i < param->philo_num)
 	{
-		pthread_create(&param->philo[i].ph, NULL, take_fork, (void *)param);
-		pthread_join(param->philo[i].ph, NULL);
+		pthread_create(&param->philo[i].ph, NULL, start_routine, (void *)&param->philo[i]);
+//		pthread_join(param->philo[i].ph, NULL);
+		pthread_detach(param->philo[i].ph);
 		i++;
 	}
+//	i = 0;
+	while(1)
+		;
+	//while (i < param->philo_num)
+	//{
+	//	pthread_detach(param->philo[i].ph);
+	//	i++;
+	//}
 }
 
 void	mutex_init(t_param *param)
@@ -53,14 +208,14 @@ void	mutex_init(t_param *param)
 void	make_philo(t_param *param)
 {
 	int	i;
+	t_param	*temp;
 
 	param->philo = (t_philo *)malloc(sizeof(t_philo) * param->philo_num);
 	i = 0;
 	while (i < param->philo_num)
 	{
-		param->philo[i].left = 0;
-		param->philo[i].right = 0;
 		param->philo[i].identifier = i + 1;
+		param->philo[i].data = param;
 		i++;
 	}
 }
@@ -69,6 +224,7 @@ int	main(int argc, char *argv[])
 {
 	t_param	param;
 
+	param.time_stamp = 0;
 	param.philo_num = ft_atoi(argv[1]);
 	make_philo(&param);
 	mutex_init(&param);
--- philo/philo.h
+++ philo/philo.h
@@ -6,7 +6,7 @@
 /*   By: jeongwle <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/26 18:40:12 by jeongwle          #+#    #+#             */
-/*   Updated: 2021/06/28 21:30:35 by jeongwle         ###   ########.fr       */
+/*   Updated: 2021/06/29 19:08:24 by jeongwle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,10 +22,8 @@
 typedef struct	s_philo
 {
 	pthread_t		ph;
-	int				right;
-	int				left;
 	int				identifier;
-	struct timeval	new_time;
+	void			*data;
 	pthread_mutex_t fork;
 }				t_philo;
 
@@ -33,10 +31,12 @@ typedef struct	s_param
 {
 	t_philo			*philo;
 	int				philo_num;
+	int				time_stamp;
 	struct timeval	old_time;
 }				t_param;
 
 int				ft_atoi(const char *str);
 void			ft_usleep(int num);
+void			print_status(t_param *param, int param_num, char *str);
 
 #endif
